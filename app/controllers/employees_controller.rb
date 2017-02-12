bcbf5c5e36cf7249ff908a90566338b918eae436
dani <dani.zaghian@gmail.com>
2017-02-12 10:46:55 -0800

    added create route/controller method, changed application controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -7,4 +7,14 @@ class EmployeesController < ApplicationController
     @employee = Employee.find_by(id: params[:id])
     render 'show.json.jbuilder'
   end
+  def create
+    @employee = Employee.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      # birthday: params[:birthday],
+      email: params[:email]
+      # ssn: params[:ssn]
+      )
+    render 'show.json.jbuilder'
+  end
 end
